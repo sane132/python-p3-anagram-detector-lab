9064c77b9e4364a8602ef65ff4269e0ec5e0c27e
Mohamed <muhamadsane114@gmail.com>
2025-08-13 12:47:41 +0300

    passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if self.is_anagram(anagram)]
+
+    def is_anagram(self, candidate):
+        candidate_lower = candidate.lower()
+        return candidate_lower != self.word and sorted(candidate_lower) == sorted(self.word)
